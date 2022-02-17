0cf55621877d93bb6c02c01ea35bf47abf9b4f37
Shawn <shawn.harrington2776@gmail.com>
2022-02-17 13:56:30 -0800

    finished darkmode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import useLocalStorage from 'use-local-storage';
+import Square from './Square';
 
 function App() {
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+  const switchTheme = () => {
+    const newtheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newtheme);
+  };
+
   return (
-    <div className="App">
-      Dark Mode
+    <div className="app" data-theme={theme}>
+      <span>Dark Mode</span>
+      <button onClick={switchTheme}>
+        Switch to {theme === 'light' ? 'Dark' : 'Light'} Theme
+      </button>
+      <Square />
     </div>
   );
 }
---
+++ src/Square.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Square.css';
+
+function Square() {
+  return (
+    <div className='square'>
+      <span>It's a Square</span>
+    </div>
+  );
+};
+
+export default Square;
